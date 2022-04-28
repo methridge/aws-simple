a5c484702b6255bb5d8969d8e87fdda7822eac07
Michael Ethridge <michael.ethridge@wwt.com>
2022-04-28 12:58:29 -0600

    Removing SG
    Signed-off-by: Michael Ethridge <michael.ethridge@wwt.com>

--- main.tf
+++ main.tf
@@ -5,6 +5,5 @@ module "ec2_instance" {
   name                        = "test-instance"
   ami                         = data.aws_ami.ubuntu.id
   associate_public_ip_address = false
-  vpc_security_group_ids      = [module.tfc_agent_sg.security_group_id]
   subnet_id                   = data.aws_subnets.all.ids[0]
 }
